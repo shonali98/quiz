880cf6cc7cd64112016f20ec62ea7490566e7e05
shonali98 <shonalinethmika998@gmail.com>
2022-02-02 19:58:51 +0530

    question

--- questions.js
+++ questions.js
@@ -120,7 +120,7 @@ let questionsjs =[
 
 ]
 
-//creating an array and passing the number, questions, options and answers -JS questions
+//creating an array and passing the number, questions, options and answers -css questions
 let questionscss =[
     {
         numb:"1)",
@@ -175,6 +175,246 @@ let questionscss =[
             "background-image",
             "background-color",
             "None of the above"
+        ]
+    },
+
+]
+
+//creating an array and passing the number, questions, options and answers -react js questions
+let questionsreactjs =[
+    {
+        numb:"1)",
+        question:"Which of the following is the correct name of React.js?",
+        answer:"All of the above",
+        options :[
+            "React",
+            "React.js",
+            "ReactJS",
+            "All of the above"
+        ]
+    },
+    {
+        numb:"2)",
+        question:" Which of the following are the advantages of React.js?",
+        answer:"All of the above",
+        options :[
+            "React.js can increase the application's performance with Virtual DOM.",
+            "React.js is easy to integrate with other frameworks such as Angular, BackboneJS since it is only a view library.",
+            "React.js can render both on client and server side.",
+            "All of the above"
+        ]
+    },
+    {
+        numb:"3)",
+        question:"Which of the following is not a disadvantage of React.js? ",
+        answer:"The JSX in React.js makes code easy to read and write.",
+        options :[
+            "React.js has only a view layer. We have put your code for Ajax requests, events and so on.",
+            "The library of React.js is pretty large",
+            "The JSX in React.js makes code easy to read and write.",
+            "The learning curve can be steep in React.js."
+        ]
+    },
+    {
+        numb:"4)",
+        question:" Which of the following command is used to install create-react-app?",
+        answer:"npm install -g create-react-app",
+        options :[
+            "npm install -g create-react-app",
+            "npm install create-react-app",
+            "npm install -f create-react-app",
+            "install -g create-react-app"
+        ]
+    },
+    {
+        numb:"5)",
+        question:" What of the following is used in React.js to increase performance?",
+        answer:"Virtual DOM",
+        options :[
+            "Original DOM",
+            "Virtual DOM",
+            "Both A and B.",
+            "None of the above."
+        ]
+    },
+
+]
+
+//creating an array and passing the number, questions, options and answers -Angular js questions
+let questionsAngularjs =[
+    {
+        numb:"1)",
+        question:"Which of the following statement is correct for AngularJS?",
+        answer:"AngularJS is a JavaScript framework",
+        options :[
+            "AngularJS is an HTML framework",
+            "AngularJS is a Java framework",
+            "AngularJS is a JavaScript framework",
+            "AngularJS is a SQL framework"
+        ]
+    },
+    {
+        numb:"2)",
+        question:"  On which of the Architectural pattern AngularJS is based?",
+        answer:"MVVM Architectural pattern",
+        options :[
+            "Observer Pattern",
+            "Decorator pattern",
+            "MVC Architecture pattern",
+            "MVVM Architectural pattern"
+        ]
+    },
+    {
+        numb:"3)",
+        question:"AngularJS is perfect for? ",
+        answer:"SPAs",
+        options :[
+            "SPAs",
+            "MPAs",
+            "DPAs",
+            "CPAs"
+        ]
+    },
+    {
+        numb:"4)",
+        question:" Which of the following command is used to install create-react-app?",
+        answer:"npm install -g create-react-app",
+        options :[
+            "npm install -g create-react-app",
+            "npm install create-react-app",
+            "npm install -f create-react-app",
+            "install -g create-react-app"
+        ]
+    },
+    {
+        numb:"5)",
+        question:" Which of the following directive is used to bind the application data to the HTML view in AngularJS?",
+        answer:"ng-bind directive",
+        options :[
+            "ng-app directive",
+            "ng-model directive",
+            "ng-bind directive",
+            "ng-init directive"
+        ]
+    },
+
+]
+
+//creating an array and passing the number, questions, options and answers -Typescript questions
+let questionsTs =[
+    {
+        numb:"1)",
+        question:"Which are the different Data Types supported by Typescript?",
+        answer:" All of the above mentioned",
+        options :[
+            "Boolean var bValue: boolean = false",
+            "Number var age: number = 16",
+            "String var name: string = 'jon'",
+            " All of the above mentioned"
+        ]
+    },
+    {
+        numb:"2)",
+        question:"  Which statements are true regarding JSX modes?",
+        answer:"The preserve mode will keep the JSX as part of the output to be further consumed by another transform step",
+        options :[
+            "These modes only affect the emit stage - type checking is unaffected",
+            "The output of react mode will have a .jsx file extension",
+            "The react mode will emit React.addElement",
+            "The preserve mode will keep the JSX as part of the output to be further consumed by another transform step"
+        ]
+    },
+    {
+        numb:"3)",
+        question:"What are variables in Typescript? ",
+        answer:"All",
+        options :[
+            "A variable name should contain alphabets and numeric digits",
+            "It cannot contain spaces and special characters except underscore (_) and dollar ($) sign",
+            "A variable name cannot begin with a digit",
+            "All"
+        ]
+    },
+    {
+        numb:"4)",
+        question:" What are the variable scopes available in TypeScript?",
+        answer:"All of the above",
+        options :[
+            "Global Scope",
+            "Class Scope",
+            "Local Scope",
+            "All of the above"
+        ]
+    },
+    {
+        numb:"5)",
+        question:" What are the benefits of Typescript?",
+        answer:"All of the above",
+        options :[
+            "It helps in code structuring",
+            "Use class based object oriented programming",
+            "Impose coding guidelines",
+            "All of the above"
+        ]
+    },
+
+]
+
+//creating an array and passing the number, questions, options and answers -.Net questions
+let questionsdotnet =[
+    {
+        numb:"1)",
+        question:"Which webserver is developed by Microsoft?",
+        answer:"Internet Information Services (IIS)",
+        options :[
+            "Apache Tomcat",
+            "Caudium",
+            "Internet Information Services (IIS)",
+            "WEBrick"
+        ]
+    },
+    {
+        numb:"2)",
+        question:"Which protocol is used to transfer files frtom localhost to remote host?",
+        answer:"FTP",
+        options :[
+            "HTTP",
+            "FTP",
+            "TCP",
+            "UDP"
+        ]
+    },
+    {
+        numb:"3)",
+        question:"Which commands are used to specify settings of an .aspx file? ",
+        answer:"Directives",
+        options :[
+            "Class",
+            "Directives",
+            "Events",
+            "Validation"
+        ]
+    },
+    {
+        numb:"4)",
+        question:" Which of the following directive is used to link an assembly to a page or user control?",
+        answer:"@Assembly",
+        options :[
+            "@Page",
+            "@Import",
+            "@Assembly",
+            "@Reference"
+        ]
+    },
+    {
+        numb:"5)",
+        question:"The @Implements directive",
+        answer:"indicates that a page or user control implements a specified .NET Framework interface",
+        options :[
+            "imports a namespace into current page page or user control",
+            "assigns a class or virtual path used to type the Master property of a page",
+            "indicates that a page or user control implements a specified .NET Framework interface",
+            "none of these above"
         ]
     },
 
---
+++ scriptreactjs.js
@@ -0,0 +1,155 @@
+//getting all required elemnts
+const start_btn =document.querySelector(".start_btn button");
+const info_box =document.querySelector(".info_box  ");
+const exit_info_btn =document.querySelector(" .info_box .buttons .exit");
+const continue_info_btn =document.querySelector(".info_box .buttons .continue  ");
+const quiz_box =document.querySelector(".quiz_box ");
+const result_box =document.querySelector(".result_box ");
+const timeCount= quiz_box.querySelector(".quiz_box .timer .timer_sec ");
+const timeline= quiz_box.querySelector(".quiz_box .time_line ");
+const exit_quiz_btn =document.querySelector(".quiz_box .buttons .exit ");
+const exit_result_btn =document.querySelector(".result_box .buttons .exit ");
+
+
+
+
+// start quiz button clicked
+start_btn.onclick = () =>{
+    info_box.classList.add("activeInfo");
+
+    
+}
+// if  exit button clicked
+exit_info_btn.onclick = () =>{
+    info_box.classList.remove("activeInfo"); // hide the info box
+}
+
+// if  exit quiz  button clicked
+exit_quiz_btn.onclick = () =>{
+    quiz_box.classList.remove("activeQuiz"); // hide the info box
+}
+// if  exit result  button clicked
+exit_result_btn.onclick = () =>{
+    result_box.classList.remove("activeResult");// hide the info box
+    quiz_box.classList.remove("activeQuiz"); // hide the info box
+    info_box.classList.add("activeInfo");
+}
+
+// if continue button clicked
+continue_info_btn.onclick = () =>{
+    info_box.classList.remove("activeInfo"); // hide the info box
+    quiz_box.classList.add("activeQuiz"); // show the quiz box
+    
+    showquestions(0);
+    startTimer(15);
+    startTimeLine(0);
+}
+
+let que_count=0;
+let counter;
+let timeValue=15;
+let widthValue=0;
+
+const next_btn= quiz_box.querySelector(" .continue ");
+// if next question select 
+next_btn.onclick =()=>{
+    
+    if(que_count < questionshtml.length-1){
+        que_count++;
+        showquestions(que_count);
+        clearInterval(counter);
+        startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimeLine(widthValue);
+    }
+    else{
+        console.log("questions complete");
+        result();
+
+    }
+    
+}
+
+
+
+// getting que and option from array
+function showquestions(index){
+    
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".quiz_box .option_list");
+    let que_tag ="<span>"+ questionsreactjs[index].numb+questionsreactjs[index].question+"</span>";
+    let option_tag='<div class="option">'+questionsreactjs[index].options[0]+'</div>'
+                      +'<div class="option">'+questionsreactjs[index].options[1]+'</div>'
+                      +'<div class="option">'+questionsreactjs[index].options[2]+'</div>'
+                      +'<div class="option">'+questionsreactjs[index].options[3]+'</div>';
+    que_text.innerHTML=que_tag;
+    option_list.innerHTML=option_tag;
+    
+    const option=option_list.querySelectorAll(".option");
+    for(let i=0; i<option.length;i++){
+        option[i].setAttribute("onclick", "optionSelected(this)")
+    }
+}
+let marks=0;
+
+function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine);
+    let userAns=answer.textContent;
+    let correctAns=questionsreactjs[que_count].answer;
+    
+
+    console.log(correctAns);
+
+    if(userAns==correctAns){
+         marks=marks+1;
+         answer.classList.add("correct");
+         console.log(marks);
+    }
+    else{
+        answer.classList.add("incorrect");
+        console.log(marks);
+
+        
+    }
+
+}
+
+function result(){
+    
+    result_box.classList.add("activeResult"); // show the quiz box
+    const scoreText= result_box.querySelector(".score_text");
+    let scoreTag='<div class="score_text">you WON '+ marks +' out of 5</div>';
+    scoreText.innerHTML=scoreTag;
+}
+
+
+function startTimer(time){
+    counter=setInterval(timer,1000);
+    function timer(){
+        timeCount.textContent=time;
+        time--;
+        if(time<9){
+            let addZero= timeCount.textContent;
+            timeCount.textContent="0" +addZero;
+        }
+        if(time < 0){
+            clearInterval(counter);
+            timeCount.textContent="00";
+        }
+
+    }
+}
+
+function startTimeLine(time){
+    counterLine=setInterval(timer,29);
+    function timer(){
+        time +=1;
+        timeline.style.width=time+"px";
+
+        if(time > 549){
+            clearInterval(counterLine);
+        }
+
+    }
+}
